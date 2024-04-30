17695c0d699e43b41b310c8c19fccf29dd5c2976
Max <bulanovich5@seznam.cz>
2024-04-30 13:30:58 +0200

    fixed js import

--- lib/src/transports/websocket_web_impl.dart
+++ lib/src/transports/websocket_web_impl.dart
@@ -1,5 +1,6 @@
 import 'dart:html';
-import 'dart:js_util' as JSUtils;
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:js_util' if (kIsWeb) 'dart:js' as JSUtils;
 
 import 'package:sip_ua/src/sip_ua_helper.dart';
 import '../logger.dart';
